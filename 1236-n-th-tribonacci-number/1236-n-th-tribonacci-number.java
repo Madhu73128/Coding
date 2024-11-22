d40a4f54d078591e46414a8d9d7079aaed874384
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-11-22 19:54:53 +0530

    Time: 0 ms (100%), Space: 40.7 MB (11.96%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -1,17 +1,15 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n<2)
+        if(n==0||n==1)
         return n;
         if(n==2)
         return 1;
-        int dp[]={0,1,1};
+        int dp[]=new int[n+1];
+        dp[0]=0;dp[1]=1;dp[2]=1;
         for(int i=3;i<=n;i++)
         {
-            int further=dp[0]+dp[1]+dp[2];
-            dp[0]=dp[1];
-            dp[1]=dp[2];
-            dp[2]=further;
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
-        return dp[2];
+        return dp[n];
     }
 }
