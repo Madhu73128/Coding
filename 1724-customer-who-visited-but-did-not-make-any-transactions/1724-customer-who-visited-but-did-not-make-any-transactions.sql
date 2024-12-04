9354e31984ec2792a5e451c25522714a2e4f2d37
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-04 19:18:24 +0530

    Time: 1531 ms (44.13%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,3 +1,6 @@
 # Write your MySQL query statement below
 select customer_id,count(*) as count_no_trans from Visits
 where visit_id not in(select visit_id from Transactions) group by customer_id;
+-- customer_id 30 doesnt make any transactions for 1 time
+-- customer_id 96 doesnt make any transactions for 1 time
+-- customer_id 54 doesnt make any transactions for 2 times
