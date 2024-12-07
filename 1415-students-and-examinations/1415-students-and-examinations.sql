ecff79484591cedd2ce94960a93f5c13a42d3ece
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-07 11:17:41 +0530

    Time: 983 ms (59.24%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -10,5 +10,5 @@ order by st.student_id,sub.subject_name;*/
 select st.student_id,st.student_name,sub.subject_name,count(ex.subject_name) as attended_exams from Students st 
 cross join Subjects sub
 left join Examinations ex on st.student_id=ex.student_id and sub.subject_name=ex.subject_name
-group by st.student_id,st.student_name,sub.subject_name 
+group by st.student_id,sub.subject_name 
 order by st.student_id,sub.subject_name;
