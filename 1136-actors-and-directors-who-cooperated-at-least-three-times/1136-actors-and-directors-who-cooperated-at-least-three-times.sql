1dd652df32f79fb172bf46b15242118ba4b7ac36
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-13 01:13:40 +0530

    Time: 584 ms (9.33%), Space: 0B (100%) - LeetHub

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -3,5 +3,9 @@
 -- group by actor_id having count(director_id)>=3)
 -- select actor_id,director_id from cte;//not working why??
 
-select actor_id,director_id from ActorDirector 
-group by actor_id,director_id having count(timestamp)>=3;
+-- select actor_id,director_id from ActorDirector 
+-- group by actor_id,director_id having count(timestamp)>=3;
+
+with cte as(select actor_id,director_id from ActorDirector 
+group by actor_id,director_id having count(timestamp)>=3)
+select actor_id,director_id from cte;#*
