8c9bbca6cb1e3ce4a5f3f00083d3a18e8e7364c8
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-24 16:17:15 +0530

    Time: 0 ms (100%), Space: 40.5 MB (53.37%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,15 +1,31 @@
 class Solution {
     public int numberOfSteps(int num) {
+        // int count=0;
+        // while(num>0)
+        // {
+        //     if(num%2==0)
+        //     {
+        //         num/=2;
+        //     }
+        //     else
+        //     {
+        //         num-=1;
+        //     }
+        //     count++;
+        // }
+        // return count;
         int count=0;
         while(num>0)
         {
-            if(num%2==0)
+            if((num&1)==0)
             {
-                num/=2;
+                num>>=1;
             }
             else
             {
-                num-=1;
+                // num^=1;
+                // num=1^num;
+                num=(num>>1)<<1;
             }
             count++;
         }
