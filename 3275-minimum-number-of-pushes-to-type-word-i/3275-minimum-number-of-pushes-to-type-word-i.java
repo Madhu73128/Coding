7ec901d9ca7d99df9e50c9d0f98aba3a0236aa7e
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-25 17:20:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (56.43%) - LeetHub

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int res=0;
+        int len=word.length();
+        int cantake=0;
+        int pushcounts=1;
+        while(len>0)
+        {
+            if(len>=8)
+            {
+                cantake=8;
+            }
+            else
+            {
+                cantake=len;
+            }
+            res+=pushcounts*cantake;
+            len=len-cantake;
+            pushcounts++;
+        }
+        return res;
+    }
+}
+// https://www.youtube.com/watch?v=ZAdVGNFaFlU
