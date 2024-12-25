a4805ea1b9588b1b09a7f20e138b0cca8d3c1e93
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-25 20:26:14 +0530

    Time: 0 ms (100%), Space: 40.3 MB (79.53%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,17 +1,29 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        return bitCount(x^y);
-    }
-    public static int bitCount(int n)
-    {
         int count=0;
-        while(n>0)
+        while(x>0||y>0)
         {
-            // n=n&(n-1);
-            // count+=1;
-            count+=(n&1);
-            n>>=1;
+            if((x&1)!=(y&1))
+            {
+                count++;
+            }
+            x>>=1;
+            y>>=1;
         }
         return count;
+        // return bitCount(x^y);
     }
+    // public static int bitCount(int n)
+    // {
+    //     int count=0;
+    //     while(n>0)
+    //     {
+    //         // n=n&(n-1);
+    //         // count+=1;
+    //         count+=(n&1);
+    //         n>>=1;
+    //     }
+    //     return count;
+    // }
 }
+// https://www.youtube.com/watch?v=eeO4btyorq8
