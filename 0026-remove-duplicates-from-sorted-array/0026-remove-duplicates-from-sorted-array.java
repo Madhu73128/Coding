f62e87201b46915f73f2311c7a6923d7b83e93e3
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-27 18:01:38 +0530

    Time: 1 ms (65.17%), Space: 44.9 MB (42.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,13 +1,32 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k=1;
-        for(int i=1;i<nums.length;i++)
+        int k=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
         {
-            if(nums[i]!=nums[i-1])
+            if(i<n-1&&nums[i]==nums[i+1])
+            {
+                continue;
+            }
+            else
             {
                 nums[k++]=nums[i];
             }
         }
         return k;
     }
+
+    // {
+    //     int k=1;
+    //     for(int i=1;i<nums.length;i++)
+    //     {
+    //         if(nums[i]!=nums[i-1])
+    //         {
+    //             nums[k++]=nums[i];
+    //         }
+    //     }
+    //     return k;
+    // }
 }
+// https://www.youtube.com/watch?v=xnj-GjdboGE
+// https://www.youtube.com/watch?v=uWFuRAHFa2M   //Doubt
