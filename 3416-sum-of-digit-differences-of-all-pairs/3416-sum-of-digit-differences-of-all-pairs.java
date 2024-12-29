db309a48741b9ccd608add49da274927e4ac7266
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2024-12-29 22:49:58 +0530

    Time: 272 ms (5.22%), Space: 56.9 MB (100%) - LeetHub

---
+++ 3416-sum-of-digit-differences-of-all-pairs/3416-sum-of-digit-differences-of-all-pairs.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long sumDigitDifferences(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,HashMap<Character,Long>> hm=new HashMap<>();
+        for(int num:nums)
+        {
+            String s=Integer.toString(num);
+            int len=s.length();
+            for(int i=0;i<len;i++)
+            {
+                hm.putIfAbsent(i,new HashMap<>());
+                hm.get(i).put(s.charAt(i),hm.get(i).getOrDefault(s.charAt(i),0L)+1);
+            }
+        }
+        long res=0;
+        for(int num:nums)
+        {
+            String s=Integer.toString(num);
+            int len=s.length();
+            for(int i=0;i<len;i++)
+            {
+                res+=n-hm.get(i).get(s.charAt(i));
+            }
+        }
+        return res/2;
+    }
+}
