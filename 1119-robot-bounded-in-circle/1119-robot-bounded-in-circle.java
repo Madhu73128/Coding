e6f3679846bfb1f4831eebc776ac4c4bfac866d2
Madhu Sudhan Reddy Takkoli <167497651+Madhu73128@users.noreply.github.com>
2025-01-28 22:46:22 +0530

    Time: 0 ms (100%), Space: 41.5 MB (49.25%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
@@ -26,4 +26,47 @@ class Solution {
     }
 }
 
+
+//     public boolean isRobotBounded(String instructions) {
+//         int angle = 0;
+//         int x = 0;
+//         int y = 0;
+//         for(char c: instructions.toCharArray())
+//         {
+//             if(c=='R')
+//                 angle = (angle-90+360)%360;
+//             else if(c=='L')
+//                 angle = (angle+90)%360;
+//             else
+//             {
+//                 System.out.println(angle);
+//                 switch (angle){
+//                     case 0:
+//                     {   
+//                         x++;
+//                         break;
+//                     }
+//                     case 90:
+//                     {    y--;
+//                         break;
+//                     }
+//                     case 180:
+//                     {    x--;
+//                         break;
+//                     }
+//                     case 270:
+//                     {    y++;
+//                         break;
+//                     }
+//                     default:
+//                         break;
+//                 }
+//             }
+//         }
+        
+//         return ((x==0 && y==0) || angle!=0);
+            
+//     }
+// }
+
 // https://www.youtube.com/watch?v=nKv2LnC_g6E
